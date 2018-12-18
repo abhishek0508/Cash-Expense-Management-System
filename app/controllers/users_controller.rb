349915b9d67486982ff6bf95ca887d5b1b07a5cc
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-18 18:12:49 +0530

    created basic layout and also created users model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+    end
+    def new
+    end
+    def create 
+        @user = User.new(user_params)
+        @user.save
+        redirect_to @user
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+    private 
+    def user_params
+       params.require(:user).permit(:name,:email,:password) 
+    end
+
+    
+end
