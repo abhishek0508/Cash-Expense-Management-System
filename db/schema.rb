d4791032b8f2ae2cf6b0273fe02fa2334486a1a9
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 13:16:22 +0530

    Added password_confirmation_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_154401) do
+ActiveRecord::Schema.define(version: 2018_12_19_074533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_154401) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "password_confirmation"
     t.index ["emailId"], name: "index_users_on_emailId", unique: true
   end
 
