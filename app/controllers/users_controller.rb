085e7229ad23380e445802b34d0a17b7a21acc35
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 13:28:01 +0530

    password_errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
     private 
     def user_params
-       params.require(:user).permit(:name,:emailId) 
+       params.require(:user).permit(:name,:emailId,:password,:password_confirmation) 
     end
 
     
