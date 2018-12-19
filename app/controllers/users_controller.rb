dc9beca31f14a2014f638e206815e60905ebfe37
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 12:55:25 +0530

    removed password field created previously

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
     private 
     def user_params
-       params.require(:user).permit(:name,:email,:password) 
+       params.require(:user).permit(:name,:email) 
     end
 
     
--- db/migrate/20181217094419_create_users.rb
+++ db/migrate/20181217094419_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :emailId
-      t.string :password
 
       t.timestamps
     end
