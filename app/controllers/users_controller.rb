69d7b75982cfec0ab334c9225617bfb19c9577b9
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 13:02:25 +0530

    Changed name & emailId

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
     private 
     def user_params
-       params.require(:user).permit(:name,:email) 
+       params.require(:user).permit(:name,:emailId) 
     end
 
     
