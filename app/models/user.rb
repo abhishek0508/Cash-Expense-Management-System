1cd06138707165e6e2ae1e1b0d74a8ca92dddae7
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 13:25:00 +0530

    has_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
 
     before_save {self.emailId = emailId.downcase}
     validates :name,  presence: true, length: { maximum: 50 }
-    validates :password, presence: true, length: { minimum: 6 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :emailId, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }   
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
