87a6df1ff61258c204d5929eb6bc12e5bb0056ce
Abhishek Gorisaria <abhishekcool0508@gmail.com>
2018-12-19 15:28:40 +0530

    SignUp Done along with Errors of the fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,16 @@ class UsersController < ApplicationController
     def index
     end
     def new
+        @user = User.new
     end
     def create 
         @user = User.new(user_params)
-        @user.save
-        redirect_to @user
+        if @user.save
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
     end
     def show
         @user = User.find(params[:id])
